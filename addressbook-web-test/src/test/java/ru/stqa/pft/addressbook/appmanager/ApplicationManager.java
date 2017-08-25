aad75541868597e990f42ef1e1090df9302f0bd2
AlexAZakharov <a3axapob@mail.ru>
2017-08-25 21:16:59 +0400

    теперь можно запускать в двух браузерах

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,22 +1,38 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-    FirefoxDriver wd;
+    WebDriver wd;
     private  SessionHelper sessionHelper;
     private  NavigationHelper navigationHelper;
     private  ContactsHelper contactsHelper;
     private  GroupHelper groupHelper;
+    private String browser;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
 
     public void init() {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+
+        if (browser == BrowserType.FIREFOX){
+            wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        } else if (browser == BrowserType.CHROME){
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.IE){
+            wd = new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         navigationHelper = new NavigationHelper(wd);
@@ -30,9 +46,7 @@ public class ApplicationManager {
         wd.quit();
     }
 
-    public GroupHelper getGroupHelper() {
-        return groupHelper;
-    }
+    public GroupHelper getGroupHelper() { return groupHelper; }
 
     public ContactsHelper getContactsHelper() {
         return contactsHelper;
--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactsHelper.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactsHelper.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.ContactsDate;
 
 public class ContactsHelper extends HelperBase {
 
-    public ContactsHelper(FirefoxDriver wd) {
+    public ContactsHelper(WebDriver wd) {
         super(wd);
     }
 
