ebe41042d63dcfab739f8910cfef737c52b35d53
AlexAZakharov <a3axapob@mail.ru>
2017-09-21 08:21:57 +0400

    Созданы конфигурационные файлы для стенда и для тестов

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -10,7 +10,8 @@ import ru.stqa.pft.addressbook.model.ContactsDate;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp() throws Exception {
