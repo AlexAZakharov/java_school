b52a1b30dfbfbaa42e7268337bb781085abfaa76
AlexAZakharov <a3axapob@mail.ru>
2017-09-24 18:58:31 +0400

    реализована проверка для удаления и создания групп через ДБ

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -37,10 +37,10 @@ public class GroupCreationTests extends TestBase {
     @Test(dataProvider = "validGroups")
     public void testsGroupCreation(GroupDate gd) {
         app.goTo().groupPage();
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         app.group().create(gd);
         assertThat(app.group().count(),equalTo(before.size()+1));
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(
                 before.withAdded(gd.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
     }
--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeleteTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeleteTests.java
@@ -13,8 +13,9 @@ public class GroupDeleteTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions(){
-        app.goTo().groupPage();
-        if ( app.group().all().size() ==0){
+
+        if ( app.db().groups().size() ==0){
+            app.goTo().groupPage();
             app.group().create(new GroupDate().withName("test1").withHeader("test3"));
         }
     }
@@ -22,11 +23,12 @@ public class GroupDeleteTests extends TestBase {
     @Test
     public void testsGroupDelete() {
 
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         GroupDate deletedGroup = before.iterator().next();
+        app.goTo().groupPage();
         app.group().delete(deletedGroup);
         assertEquals(app.group().count(),before.size()-1);
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(before.withOut(deletedGroup)));
 
     }
