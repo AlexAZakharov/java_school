22d9345c97def99c6e79eede1569a91552ef51a2
AlexAZakharov <a3axapob@mail.ru>
2017-09-26 11:51:44 +0400

    Добавлена проверка UI В модификацию группы

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -7,6 +7,13 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.ContactsDate;
+import ru.stqa.pft.addressbook.model.GroupDate;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -21,6 +28,18 @@ public class TestBase {
     @AfterSuite
     public void tearDown() {
         app.stop();
+    }
+
+    public void verifuGroupListInUi() {
+
+        if(Boolean.getBoolean("verifyUi")){
+            Groups dbGroupps = app.db().groups();
+            Groups uiGroupps = app.group().all();
+            assertThat(uiGroupps, equalTo(dbGroupps.stream().map((g)->
+                    new GroupDate().withId(g.getId()).withName(g.getName()))
+                    .collect(Collectors.toSet())));
+        }
+
     }
 
 }
